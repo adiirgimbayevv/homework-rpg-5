73546fe7bd3ffec5707d3ba6a830f91e38c6020d
adiirgimbayevv <adigordienko@gmail.com>
2026-03-16 10:27:11 +0500

    added reward logic based on rounds

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,19 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
-        }
-        return "TODO";
-    }
+        if (battleResult==null||battleResult.getWinner()==null) {
+            return "Error, no data";}
+        if (battleResult.getWinner().contains("Malenia")) {
+            return "Defeat: No rewards";}
+        int rounds=battleResult.getRounds();
+
+        if (rounds<=3){
+            return "Mythic Loot + 5k Gold";
+        } else if (rounds<=7) {
+            return "Rare Sword + 1k Gold";
+        } else if (rounds<=12){
+            return "Common Sack + 200 Gold";
+        } else {
+            return "Rusty Key + 10 Gold";
+        }}
 }
